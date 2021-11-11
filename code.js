9935c848562ae0e0a8acc5163744cfcc48bd14ed
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-11-11 11:16:03 -0700

    stopwatch works for hours

--- code.js
+++ code.js
@@ -45,7 +45,7 @@ reset.addEventListener('click', function () { stopwatch.reset(); });
 const timer = document.getElementById('stopwatch');
 
 var hr = 0;
-var min = 59;
+var min = 0;
 var sec = 0;
 var stoptime = true;
 
--- timer2.js
+++ timer2.js
@@ -1,7 +1,7 @@
 const timer = document.getElementById('stopwatch');
 
 var hr = 0;
-var min = 59;
+var min = 0;
 var sec = 0;
 var stoptime = true;
 
