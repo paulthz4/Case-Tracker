4035dbdef8b6335f2fede616500ee8a657cc4c3b
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-11-11 11:20:39 -0700

    fixed time formating

--- code.js
+++ code.js
@@ -1,47 +1,5 @@
 // setup for cleaner code
-let second = 0;
-const start = document.getElementById('start');
-const pause = document.getElementById('pause');
-const reset = document.getElementById('reset');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-
-// this code should regulate the counting of seconds
-
-/*const stopwatch = {
-    second: 0,
-    start: function () {
-        if (!this.interval) {
-            const self = this;
-            function count(val) {
-                return val > 9 ? val : '0' + val;
-            }
-            this.interval = setInterval(function () {
-                self.second += 1;
-                seconds.innerHTML = count(++second % 60);
-                minutes.innerHTML = count(parseInt(second / 60, 10));
-            }, 1000);
-        }
-    },
-    pause: function () {
-        clearInterval(this.interval);
-        delete this.interval;
-    },
-    reset: function () {
-        clearInterval(this.interval);
-        second = 0;
-        minutes.innerHTML = '00';
-        seconds.innerHTML = '00';
-        delete this.interval;
-    }
-};
-
-// Event listeners for each button
-start.addEventListener('click', function () { stopwatch.start(); });
-pause.addEventListener('click', function () { stopwatch.pause(); });
-reset.addEventListener('click', function () { stopwatch.reset(); });
-*/
-
+// timer code from https://dev.to/gspteck/create-a-stopwatch-in-javascript-2mak
 const timer = document.getElementById('stopwatch');
 
 var hr = 0;
@@ -110,7 +68,7 @@ var btn = document.getElementById("case-btn");
 btn.addEventListener('click',
     function () {
         var time = document.getElementById("time");
-        time.textContent = document.getElementById("minutes").textContent +" : "+ document.getElementById("seconds").textContent;
+        time.textContent = document.getElementById("stopwatch").textContent;
     });
 
 let btn2 = document.getElementById("delete");
@@ -145,14 +103,14 @@ function changeContent() {
             });
         node.appendChild(b);
         var span2 = document.createElement("SPAN");
-        span2.textContent = "00:00";
+        span2.textContent = "00:00:00";
         node.appendChild(span2);
         btn.addEventListener('click',
             function (e) {
                 //var a = document.getElementById("case-container").lastElementChild;
                 var a = e.target.parentElement;
                 var time = a.parentElement.lastChild;
-                time.textContent = document.getElementById("minutes").textContent + " : " + document.getElementById("seconds").textContent;
+                time.textContent = document.getElementById("stopwatch").textContent;
             });
         
         container.appendChild(node);
